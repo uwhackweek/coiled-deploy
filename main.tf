45a855f92eab81c490928b84bab146aeb8e0a74f
Scott Henderson <scottyh@uw.edu>
2021-05-05 16:02:58 +1000

    directly attach policy instead of role

--- main.tf
+++ main.tf
@@ -45,6 +45,17 @@ resource "aws_iam_access_key" "coiled-actor" {
   user = aws_iam_user.coiled-actor.name
 }
 
+# More advanced: all required coiled permissions (read from sidecar file)
+resource "aws_iam_policy" "coiled-permissions" {
+  name   = "coiled-permissions"
+  policy = file("coiled-permissions.json")
+}
+
+resource "aws_iam_user_policy_attachment" "coiled-permissions" {
+  user       = aws_iam_user.coiled-actor.name
+  policy_arn = aws_iam_policy.coiled-permissions.arn
+}
+
 resource "aws_iam_policy" "coiled-actor" {
   name        = "coiled-actions-user-policy"
   description = "Allow coiled user to assume role"
@@ -94,48 +105,4 @@ resource "aws_iam_role" "coiled-role" {
 	]
 }
 EOF
-}
-
-# Permisions for what the role can do once assumed
-# Simple test: access ti files in a specific S3 bucket
-resource "aws_iam_policy" "coiled-tfs3" {
-  name   = "coiled-role-tfs3"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "s3:*"
-      ],
-      "Effect": "Allow",
-      "Resource": [
-        "${data.aws_s3_bucket.terraform_state_bucket.arn}",
-        "${data.aws_s3_bucket.terraform_state_bucket.arn}/*"
-      ]
-    }
-  ]
-}
-EOF
-}
-resource "aws_iam_role_policy_attachment" "coiled-tfs3" {
-  role       = aws_iam_role.coiled-role.name
-  policy_arn = aws_iam_policy.coiled-tfs3.arn
-}
-
-# Attach an AWS-managed policy for doing anything with S3
-#resource "aws_iam_role_policy_attachment" "AmazonS3FullAccess" {
-#  role       = aws_iam_role.coiled-role.name
-#  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
-#}
-
-# More advanced: all required coiled permissions (read from sidecar file)
-resource "aws_iam_policy" "coiled-permissions" {
-  name   = "coiled-permissions"
-  policy = file("coiled-permissions.json")
-}
-
-resource "aws_iam_role_policy_attachment" "coiled-permissions" {
-  role       = aws_iam_role.coiled-role.name
-  policy_arn = aws_iam_policy.coiled-permissions.arn
 }
